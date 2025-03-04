c04459ab3659918c6d5e7291405947c95421b6ec
Sushmadepaul <144460531+Sushmadepaul@users.noreply.github.com>
2025-03-04 15:21:39 -0600

    Resolved merge conflict in App.js by adding LoginJobs and TaskerDashboardPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-
 import theme from "./styles/theme"; // Ensure you have a theme file for Material UI
 import Navbar from "./components/common/Navbar"; // Navbar component with the login button
 import Footer from "./components/common/Footer"; // Footer component
@@ -9,9 +8,8 @@ import Home from "./pages/Home"; // Home page
 import Login from "./pages/Login"; // Customer Login page
 import Signup from "./pages/Signup"; // Signup page
 import PostJob from "./pages/PostJob"; // Post job page
-import LoginJobs from "./pages/LoginJobs"; // NEW Worker Login Page
-import TaskerDashboardPage from "./pages/TaskerDashboardPage";
-
+import LoginJobs from "./pages/LoginJobs"; // Worker Login Page
+import TaskerDashboardPage from "./pages/TaskerDashboardPage"; // Tasker Dashboard Page
 
 function App() {
   return (
@@ -27,8 +25,8 @@ function App() {
               <Route path="/login" element={<Login />} /> {/* Customer Login route */}
               <Route path="/signup" element={<Signup />} /> {/* Signup route */}
               <Route path="/post-job" element={<PostJob />} /> {/* Post job route */}
-              <Route path="/login-jobs" element={<LoginJobs />} /> {/* NEW Worker Login route */}
-              <Route path="/tasker-dashboard" element={<TaskerDashboardPage />} />
+              <Route path="/login-jobs" element={<LoginJobs />} /> {/* Worker Login route */}
+              <Route path="/tasker-dashboard" element={<TaskerDashboardPage />} /> {/* Tasker Dashboard route */}
             </Routes>
           </main>
           <Footer /> {/* Footer component, remains at the bottom */}
