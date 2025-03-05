d57106bf17ef013d89e2ff7581e8f1c6be941027
lajja-desai <ldesai2@depaul.edu>
2025-03-04 22:44:42 -0600

    task form ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-
 import theme from "./styles/theme"; // Ensure you have a theme file for Material UI
 import Navbar from "./components/common/Navbar"; // Navbar component with the login button
 import Footer from "./components/common/Footer"; // Footer component
@@ -11,7 +10,8 @@ import Signup from "./pages/Signup"; // Signup page
 import PostJob from "./pages/PostJob"; // Post job page
 import LoginJobs from "./pages/LoginJobs"; // NEW Worker Login Page
 import TaskerDashboardPage from "./pages/TaskerDashboardPage";
-
+import Chat from "./components/Chat";
+import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
@@ -29,6 +29,8 @@ function App() {
               <Route path="/post-job" element={<PostJob />} /> {/* Post job route */}
               <Route path="/login-jobs" element={<LoginJobs />} /> {/* NEW Worker Login route */}
               <Route path="/tasker-dashboard" element={<TaskerDashboardPage />} />
+              <Route path="/chat" element={<Chat currentUser="User1" chatPartner="User2" />} />
+              <Route path="/task-form/:service" element={<TaskForm />} />
             </Routes>
           </main>
           <Footer /> {/* Footer component, remains at the bottom */}
@@ -39,3 +41,4 @@ function App() {
 }
 
 export default App;
+
